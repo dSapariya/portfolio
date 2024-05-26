b93af1526e64b6d02a2a9a8f39987db9e2f3fcfe
Dixa Sapariya <dixasapariya786@gmail.com>
2024-05-26 18:44:03 +0530

    nav bar design changes

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get header element
     var header = document.getElementById("header");
     
-    // Get the offset position of the header
     var sticky = header.offsetTop;
-    // Add scroll event listener
     window.addEventListener("scroll", function() {
-        // If the user scrolls down, add "sticky" class to header
         console.log('content');
         if (window.pageYOffset > sticky) {
             header.classList.add("sticky");
         } else {
-            // Otherwise, remove the "sticky" class
             header.classList.remove("sticky");
         }
     });
-    // window.addEventListener('scroll', function()  {
-    //     const content = document.getElementById('content-desc');
-    //     console.log('content',content);
-    //     const triggerPoint = window.innerHeight * 0.7; // Adjust as needed
-        
-    //     // If scrolled past 70% of the viewport height
-    //     if (window.scrollY > triggerPoint) {
-    //         console.log('in hsw')
-    //       content.classList.add('show'); // Show content
-    //     }
-    //   });
-    
-    // Smooth scroll to section on click
+
     var navLinks = document.querySelectorAll("#header #nav #nav-bar ul li a");
     navLinks.forEach(function(navLink) {
         navLink.addEventListener("click", function(e) {
